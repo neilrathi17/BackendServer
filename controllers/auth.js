3915ba6b7ae2118f42cd29d164ec2b1787466f69
unknown <neilrathi17@gmail.com>
2023-03-26 21:57:47 +0530

    auth fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,23 +43,19 @@ export const register = async (req,res)=>{
 
 //-------------------login------------------
 
-export const login= async(req,res)=>{
-    try{
-        const {email,password}=req.body;
-        const user= await User.findOne({email:email});
-        if(!user) 
-        return res.status(400).json({msg:"user does not exist"});
-
-        const ismatch = await bcrypt.compare(password,userPassowrd);
-        if(!ismatch) return res.status(400).json({msg:"invalid creds"});
-
-        const token =jwt.sign({id:user._id},process.env.JWT_SECRET)
-        
-        delete user.password;
-        res.status(200).json({token,user});
-
-    }catch(err){
-        res.status(500).json({error:err.message});
+export const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (!user) return res.status(400).json({ msg: "User does not exist. " });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
-
-}
+  };
